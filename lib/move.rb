d273a996cc5b895268636294bab9d05f95307a48
JULIA ZHOU <jzhouerm@hotmail.com>
2020-05-16 04:10:42 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,3 @@
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,15 +6,15 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  input.to_i - 1  
+def input_to_index(user_input)
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
 end
 
-board = Array.new(9, " ")
-
-def move(board, index, token = "X")
-  board[index] = token
-  index = input_to_index(input)
+def move(board, index, character = "X")
+  board[index] = character
+  return board
 end
 
 
